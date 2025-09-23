fd2764931bcac16a1102b8d01b93ead235e0789b
frectonz <fraol0912@gmail.com>
2025-09-23 10:01:02 +0300

    feat: setup tracing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let filter = std::env::var("RUST_LOG")
+        .unwrap_or_else(|_| "tracing=info,warp=debug,r2squeeze=debug".to_owned());
+
+    tracing_subscriber::fmt()
+        .with_env_filter(filter)
+        .with_span_events(tracing_subscriber::fmt::format::FmtSpan::CLOSE)
+        .init();
+
+    tracing::info!("Hello, world!");
 }
