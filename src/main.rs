129b3bfa48147657dbf946ad10ed7a84402d943a
frectonz <fraol0912@gmail.com>
2025-09-23 10:32:44 +0300

    feat: setup object storage struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+use aws_sdk_s3 as s3;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The R2 bucket to connect to.
+    #[arg(short, long)]
+    bucket: String,
+
+    /// The R2 account ID.
+    #[arg(short, long)]
+    account_id: String,
+
+    /// The R2 access key ID.
+    #[arg(short, long)]
+    key_id: String,
+
+    /// The R2 access key secret.
+    #[arg(short, long)]
+    secret: String,
+}
+
+struct ObjectStorage {
+    bucket: Box<str>,
+    client: s3::Client,
+}
+
+impl ObjectStorage {
+    async fn new() -> Self {
+        let args = Args::parse();
+
+        let config = aws_config::defaults(aws_config::BehaviorVersion::latest())
+            .endpoint_url(format!(
+                "https://{}.r2.cloudflarestorage.com",
+                args.account_id
+            ))
+            .credentials_provider(aws_sdk_s3::config::Credentials::new(
+                args.key_id,
+                args.secret,
+                None,
+                None,
+                "R2",
+            ))
+            .region("auto")
+            .load()
+            .await;
+
+        Self {
+            bucket: args.bucket.into(),
+            client: s3::Client::new(&config),
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     let filter = std::env::var("RUST_LOG")
@@ -7,6 +62,8 @@ async fn main() -> color_eyre::Result<()> {
         .with_env_filter(filter)
         .with_span_events(tracing_subscriber::fmt::format::FmtSpan::CLOSE)
         .init();
+
+    let storage = ObjectStorage::new().await;
 
     tracing::info!("Hello, world!");
 
